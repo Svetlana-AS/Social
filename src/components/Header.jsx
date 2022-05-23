56fbd6593c6a93d3efc3893fde1f856194f8b034
Svetlana Astanina <astaninasv01@gmail.ru>
2022-05-23 16:50:02 +0300

    Main components

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Header = () => {
+    return <header className='header'>
+        <img src='https://st.depositphotos.com/1008709/4676/i/950/depositphotos_46768767-stock-photo-hands-in-art-logo.jpg'></img>
+
+    </header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
+        </div>
+        <div>
+            <img src='https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg' />
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    1 post
+                </div>
+                <div>
+                    2 post
+                </div>
+            </div>
+
+        </div>
+    </div>
+}
+export default Profile;
